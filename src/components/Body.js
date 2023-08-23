e9c0c87a53a0882807d182fd6ac6cf80764061ba
avinash raut <avinashraut2409@gmail.com>
2023-08-23 17:45:36 +0530

    shimmer updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,27 +13,21 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
+
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
-    console.log(json);
+
 
     setListOfRestaurant(
       json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
-    setFilteredRestaurant( json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurant(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    )
   };
-
-  //   setListOfRestraunt(
-  //   json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-  //   setFilteredRestaurant(
-  //     json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-  //   );
-  // };
-   
- // console.log(listOfRestaurant);
 
   return listOfRestaurant.length === 0 ? (
     <Shimmer />
@@ -51,9 +45,9 @@ const Body = () => {
           />
           <button onClick={() => {
             const filteredRestaurant = listOfRestaurant.filter(
-              (res)=> res.info?.name.toLowerCase().includes(searchText.toLowerCase())
+              (res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase())
             );
-             setListOfRestaurant(filteredRestaurant);
+            setFilteredRestaurant(filteredRestaurant);
 
           }}>Search</button>
         </div>
@@ -61,9 +55,9 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurant.filter(
-              (res) => res.info?.avgRating > 4
+              (res) => res.info.avgRating >  4.3
             );
-            setListOfRestaurant(filteredList);
+            setFilteredRestaurant(filteredList);
           }}
         >
           Top Rated Restraunts
